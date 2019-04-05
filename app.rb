a666b92a2c10f537a508f62bf4c0f1e97cc991e2
J. Singleton <jennifer.singleton@gmail.com>
2019-04-05 22:25:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   # Define your GET '/' route below and respond with "Hello, World!"
-  GET '/' do
+  get '/' do
     "oh, hay there."
   end
 
